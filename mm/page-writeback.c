b7aa02cf13b9e4813c53f9f44e5974cce2586916
Raphiel Rollerscaperers <raphielscape@outlook.com>
2018-09-19 19:54:38 +0700

    mm,dcache: memory-management stuffs
    > cache-pressure at 50
    > dirty-background at 15
    > dirty-writeback and dirty-expire in 100 centiseconds
    > enable process reclaim
    
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -70,7 +70,7 @@ static long ratelimit_pages = 32;
 /*
  * Start background writeback (via writeback threads) at this percentage
  */
-int dirty_background_ratio = 10;
+int dirty_background_ratio = 15;
 
 /*
  * dirty_background_bytes starts at 0 (disabled) so that it is a function of
@@ -98,14 +98,14 @@ unsigned long vm_dirty_bytes;
 /*
  * The interval between `kupdate'-style writebacks
  */
-unsigned int dirty_writeback_interval = 5 * 100; /* centiseconds */
+unsigned int dirty_writeback_interval = 1 * 100; /* centiseconds */
 
 EXPORT_SYMBOL_GPL(dirty_writeback_interval);
 
 /*
  * The longest time for which data is allowed to remain dirty
  */
-unsigned int dirty_expire_interval = 30 * 100; /* centiseconds */
+unsigned int dirty_expire_interval = 1 * 100; /* centiseconds */
 
 /*
  * Flag that makes the machine dump writes/reads and block dirtyings.
