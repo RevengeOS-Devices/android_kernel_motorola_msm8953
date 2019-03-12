f1217e896321d71f1046b4c920a0f7f33f9a346f
utsavisgreat <utsavbalar1231@gmail.com>
2019-03-12 18:24:45 +0000

    KLAPSE : Fixes & add Brightness Scaling Factor
    Refrence - https://github.com/ChimeraKernelProject/chimera_land-current/commit/f07e08f0b0754b1d4a75cb031c29cd70075b887d#diff-e7beb2a4404851c310020497996983d0
    
    thanks @rupansh

--- drivers/video/msm/mdss/klapse.h
+++ drivers/video/msm/mdss/klapse.h
@@ -4,15 +4,36 @@
 /* Required variables for external access. Change as per use */
 extern void set_rgb_slider(u32 bl_lvl);
 
-// This file uses generalised K_### defines
-// The interpretation (right argument) should be the respective color's var that you
-// include as extern via the CUSTOM_HEADER above
-#define K_RED    kcal_red
-#define K_GREEN  kcal_green
-#define K_BLUE   kcal_blue
+// from mdss kcal
+extern void kcal_klapse_push(int r, int g, int b);
 
-#define K_TYPE   unsigned short
+/* DEFAULT_ENABLE values :
+ * 0 = off
+ * 1 = time-based scaling
+ * 2 = brightness-based scaling
+ */
+#define DEFAULT_ENABLE  0
 
-extern K_TYPE K_RED, K_GREEN, K_BLUE;
+// MAX_SCALE : Maximum value of RGB possible
+#define MAX_SCALE       256
+
+// SCALE_VAL_MIN : Minimum value of RGB recommended
+#define SCALE_VAL_MIN   20
+
+// MAX_BRIGHTNESS : Maximum value of the display brightness/backlight
+#define MAX_BRIGHTNESS  255
+
+// MIN_BRIGHTNESS : Minimum value of the display brightness/backlight
+#define MIN_BRIGHTNESS  5
+
+/* UPPER_BL_LVL : Initial upper limit for brightness-dependent mode.
+ * Value <= MAX_BRIGHTNESS && > LOWER_BL_LVL (MUST)
+ */
+#define UPPER_BL_LVL  255
+
+/* LOWER_BL_LVL : Initial lower limit for brightness-dependent mode.
+ * Value < UPPER_BL_LVL (MUST)
+ */
+#define LOWER_BL_LVL 5
 
 #endif  /* _LINUX_KLAPSE_H */
