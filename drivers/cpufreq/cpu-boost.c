7e23d1348ac929f4b0014b9dbd3d1d61ed14612c
utsavisgreat <utsavbalar1231@gmail.com>
2019-05-18 13:40:06 +0000

    cpu-boost: reduce duration to 128ms
    Signed-off-by: utsavisgreat <utsavbalar1231@gmail.com>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -38,7 +38,7 @@ static struct kthread_work input_boost_work;
 static unsigned int input_boost_enabled = 1;
 module_param(input_boost_enabled, uint, 0644);
 
-static unsigned int input_boost_ms = 1500;
+static unsigned int input_boost_ms = 128;
 module_param(input_boost_ms, uint, 0644);
 
 static bool sched_boost_on_input;
